cb882844748a5f9b939597d299bb248e762368bf
henryzhouu <henry.zhou26@ycdsbk12.ca>
2024-10-07 10:43:03 -0400

    Add files via upload

---
+++ 4.2lesson.py
@@ -0,0 +1,48 @@
+# count = 1
+# total = 0
+# while count <= 10:
+#     total = count + total
+#     count +=1
+# print(total)
+
+# count = 100
+# total = 0
+# while 100 <= count <= 200:
+#     total = count + total
+#     count += 1
+# print(total)
+
+# count = 100
+# total = 0
+# count2 = 200
+# total2 = 0
+
+# while count <= 200:
+#     total = count + total
+#     count += 1
+
+# while count2 <= 300:
+#     total2 = count2 + total2
+#     count2 += 1
+
+# print(total2 - total)
+
+# count = 1000
+# total = 0
+
+# while count <= 10000:
+#     if count % 5 == 0:  # Check if it's a multiple of 5
+#         total += count
+#     count += 1
+
+# print(total)
+
+# count = 1
+# total = 0
+
+# while count <= 100:
+#     if count % 5 == 0 and count % 3 != 0:  # Multiple of 5 but not multiple of 3
+#         total += count
+#     count += 1
+
+# print(total)
